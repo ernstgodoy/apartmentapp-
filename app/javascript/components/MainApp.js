aa2dafd5ff472ea457f2c13152e48ef064d5bd4e
ernesto <ernsto.godoy@gmail.com>
2020-09-24 11:59:38 -0700

    edits

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -2,7 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import { Jumbotron } from 'reactstrap'
 import Example from "./Nav"
-
 //pages
 import AllApartments from "./pages/AllApartments"
 import Apartments from "./pages/Apartments"
